73ceb237940328e8effd1d9acfbaa2cd733ddb48
cethien <borislaw.sotnikow@gmx.de>
2025-01-27 14:14:42 +0100

    sveltekit: generative project

--- sveltekit-project/flake.nix
+++ sveltekit-project/flake.nix
@@ -15,13 +15,26 @@
         ];
 
         shellHook = ''
-          if [ ! -d node_modules ]; then
-              bun install
-          fi
-
           if [ ! -d .git ]; then
               git init && git add . && echo "chore: init" > .git/COMMIT_EDITMSG
               lefthook install
+          fi
+
+          if [ ! -f svelte.config.js ]; then
+              bunx sv create
+          fi
+
+          if [ -f vite.config.ts_INIT ] && [ -f vite.config.ts ]; then
+            rm vite.config.ts &&
+                mv vite.config.ts_INIT vite.config.ts
+          fi
+
+          if [ ! -d node_modules ] && [ -f package.json ]; then
+              bun install
+          fi
+
+          if [ ! -f .env ] && [ -f .env.example ]; then
+              cp .env.example .env
           fi
         '';
       };
--- sveltekit-project/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<article class="prose dark:prose-invert xl:prose-xl">
-	<h1>Hi 👋</h1>
-
-	<p>
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae eum
-		doloribus quidem. Cupiditate autem nisi consequuntur, neque quibusdam rem
-		ullam iste aliquid quod animi nesciunt dolores nihil labore similique ad.
-	</p>
-</article>
